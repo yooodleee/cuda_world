502234330f22f19d2e9dfb9e9461097941594c0c
yooodleee <accia25@naver.com>
2025-11-22 14:11:27 +0900

    sum of vectors

---
+++ cuda_world/basic/AddVector.cu
@@ -0,0 +1,70 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_DATA 1024		/* The size of the vector */
+
+__global__ void vecAdd(int* _a, int* _b, int* _c) {		/* Simple vector sum kernel (Max vector size: 1024) */
+	int tID = threadIdx.x;
+	_c[tID] = _a[tID] + _b[tID];
+}
+
+int main(void) {
+	int* a, * b, * c, * hc;		/* Vectors on the host */
+	int* da, * db, * dc;		/* Vectors on the device */
+
+	int memSize = sizzeof(int) * NUM_DATA;
+	printf("%d element, memSize = %d bytes\n", NUM_DATA, memSize);
+
+	/* Memory allocation on the host-side */
+	a = new int[NUM_DATA]; memset(a, 0, memSize);
+	b = new int[NUM_DATA]; memset(b, 0, memSize);
+	c = new int[NUM_DATA]; memset(c, 0, memSize);
+	hc = new int[NUM_DATA]; memset(hc, 0, memSize);
+
+	for (int i = 0; i < NUM_DATA; i++) {		/* Data generation */
+		a[i] = rand() % 10;
+		b[i] = rand() % 10;
+	}
+
+	for (int i = 0; i < NUM_DATA; i++)			/* Vectors sum on host (for performance comparison) */
+		hc[i] = a[i] + b[i];
+
+	/* Memory allocation on the device-side */
+	cudaMalloc(&da, memSize); cudaMemset(da, 0, memSize);
+	cudaMalloc(&db, memSize); cudaMemset(db, 0, memSize);
+	cudaMalloc(&dc, memSize); cudaMemset(dc, 0, memSize);
+
+	/* Data copy : Host -> Device */
+	cudaMemcpy(da, a, memSize, cudaMemcpyHostToDevice);
+	cudaMemcpy(db, b, memSize, cudaMemcpyHostToDevice);
+
+	/* Kernel call */
+	vecAdd << <1, NUM_DATA >> > (da, db, dc);
+
+	/* Copy results: Device -> Host */
+	cudaMemcpy(c, dc, memSize, cudaMemcpyDeviceToHost);
+
+	/* Release device memory */
+	cudaFree(da); cudaFree(db); cudaFree(dc);
+
+	/* Check results */
+	bool result = true;
+	for (int i = 0; i < NUM_DATA; i++) {
+		if (hc[i] != c[i]) {
+			printf("[%d] The result is not matched! (%d, %d)\n", i, hc[i], c[i]);
+			result = false;
+		}
+	}
+
+	if (result)
+		printf("GPU works well!\n");
+
+	/* Release host memory */
+	delete[] a; delete[] b; delete[] c;
+
+	return 0;
+}
