4a22bd31a84360baf2f8df25b6d63914933a7d76
yooodleee <accia25@naver.com>
2025-11-23 20:48:49 +0900

    set thread-layout

---
+++ cuda_world/thread/SetThreadLayout.cu
@@ -0,0 +1,38 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+__global__ void checkIndex(void) {
+	printf("threadIdx:(%d, %d, %d) blockIdx:(%d, %d, %d) blockDim:(%d, %d, %d) gridDim:(%d, %d, %d)\n",
+		threadIdx.x, threadIdx.y, threadIdx.z,
+		blockIdx.x, blockIdx.y, blockIdx.z,
+		blockDim.x, blockDim.y, blockDim.z,
+		gridDim.x, gridDim.y, gridDim.z);
+}
+
+int main(void) {
+	dim3 dimBlock(3, 1, 1);		/* or dimBlock(3) */
+	dim3 dimGrid(2, 1, 1);		/* or dimGrid(2) */
+
+	printf("dimGrid.x=%d dimGrid.y=%d dimGrid.z=%d\n", dimGrid.x, dimGrid.y, dimGrid.z);
+	printf("dimBlock.x=%d dimBlock.y=%d dimBlock.z=%d\n", dimBlock.x, dimBlock.y, dimBlock.z);
+
+	checkIndex << <dimGrid, dimBlock >> > ();
+
+	return 0;
+}
+
+// Output:
+/*
+dimGrid.x=2 dimGrid.y=1 dimGrid.z=1
+dimBlock.x=3 dimBlock.y=1 dimBlock.z=1
+threadIdx:(0, 0, 0) blockIdx:(1, 0, 0) blockDim:(3, 1, 1) gridDim:(2, 1, 1)
+threadIdx:(1, 0, 0) blockIdx:(1, 0, 0) blockDim:(3, 1, 1) gridDim:(2, 1, 1)
+threadIdx:(2, 0, 0) blockIdx:(1, 0, 0) blockDim:(3, 1, 1) gridDim:(2, 1, 1)
+threadIdx:(0, 0, 0) blockIdx:(0, 0, 0) blockDim:(3, 1, 1) gridDim:(2, 1, 1)
+threadIdx:(1, 0, 0) blockIdx:(0, 0, 0) blockDim:(3, 1, 1) gridDim:(2, 1, 1)
+threadIdx:(2, 0, 0) blockIdx:(0, 0, 0) blockDim:(3, 1, 1) gridDim:(2, 1, 1)
+*/
