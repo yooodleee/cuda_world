d8bed9e615158d97019ee467d42010b96c0b1b6c
yooodleee <accia25@naver.com>
2026-01-06 19:16:47 +0900

    atomic operation and syncwarp api

---
+++ cuda_world/sync/syncwarp.cu
@@ -0,0 +1,30 @@
+// for __syncthreads()
+#ifndef __CUDACC__
+#define __CUDACC__
+#endif
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "cuda.h"
+#include "device_functions.h"
+#include "cuda_runtime_api.h"
+
+#include <stdio.h>
+
+#define BLOCK_SIZE 64
+
+__global__ void syncWarp_test() {
+	int tID = threadIdx.x;
+	int warpID = (int)(tID / 32);
+	__shared__ int masterID[BLOCK_SIZE / 32];
+
+	if (threadIdx.x % 32 == 0) {
+		masterID[warpID] = tID;
+	}
+	__syncwarp();	// intra-warp synchronization (barrier)
+
+	printf("[t%d] The master of our warp is t%d\n", tID, masterID[warpID]);
+}
+
+int main() {
+	syncWarp_test << <1, BLOCK_SIZE >> > ();
+}
